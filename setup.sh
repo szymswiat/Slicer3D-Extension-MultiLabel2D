226d88b88a2d0941d2ac16f4a286ea6dc58025fa
Szymswiat <szymswiat5@gmail.com>
2021-11-27 18:36:42 +0100

    Add support for new layout of mimage_utils repo

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm $PWD/CxrAISlicer/SegmentEditorMultiLabel2D/mimage_utils
+
+ln -s $PWD/CxrAISlicer/SegmentEditorMultiLabel2D/submodules/mimage_utils/mimage_utils \
+      $PWD/CxrAISlicer/SegmentEditorMultiLabel2D/mimage_utils
